e6de9bf1bcb24e2b5d87aca2808cb676eb4f0ff9
Samuel Deinum <sam.c.deinum@gmail.com>
2019-06-13 14:44:17 -0400

    Fix Functional Bugs with check.js

--- src/containers/App/Game/check.js
+++ src/containers/App/Game/check.js
@@ -1,4 +1,7 @@
 class Check {
+  // TODO: WALL Function
+  // TODO: WORKS but repeats same loop twice
+
   constructor(grid, sX, sY, turn) {
     this.grid = grid;
     this.sX = sX;
@@ -7,13 +10,13 @@ class Check {
     this.turn = turn;
     this.dx = [-1, -1, -1, 0, 1, 1, 1, 0];
     this.dy = [1, 0, -1, -1, -1, 0, 1, 1];
-    this.last = null;
+    this.last = {};
   }
 
   calc() {
-    const id = this.sX.toString() + this.sY.toString();
+    const id = this.sX * 10 + this.sY;
     let ht = {};
-    ht[id] = [this.sX, this.sY];
+    ht[id] = Object.keys(ht).length;
     this.deep(this.sX, this.sY, ht, "");
   }
 
@@ -22,73 +25,79 @@ class Check {
       const mX = x + this.dx[i];
       const mY = y + this.dy[i];
       const cHt = { ...ht };
-      //console.log("DEEP", mX, mY, prev);
-      //console.log(cHt);
       if (
         this.grid[mX][mY] === this.turn &&
-        mX.toString() + mY.toString() != prev &&
-        mX.toString() + mY.toString() != this.last
+        mX * 10 + mY != prev //&&
+        //!(mX * 10 + mY in this.last)
       ) {
-        if (mX.toString() + mY.toString() in cHt) {
-          this.fill(cHt);
+        if (mX * 10 + mY in cHt) {
+          this.fill(cHt, mX * 10 + mY, x * 10 + y);
         } else {
-          cHt[mX.toString() + mY.toString()] = [mX, mY];
-          this.deep(mX, mY, cHt, x.toString() + y.toString());
+          cHt[mX * 10 + mY] = Object.keys(cHt).length;
+          this.deep(mX, mY, cHt, x * 10 + y);
         }
       }
     }
   }
 
-  fill(ht) {
-    //console.log("FIIIIIIIIIIIIIIIIIIILL");
-    const k = Object.keys(ht);
+  fill(ht, trig, last) {
+    //Usefull Var
+    const nHt = {};
     let full = true;
     const del = [];
 
-    this.last = k[k.length - 1];
-    for (let i = 1; i < k.length; i++) {
-      if (ht[k[i]][0] == ht[k[i - 1]][0]) {
-        let max = null;
-        let min = null;
-        if (ht[k[i]][1] >= ht[k[i - 1]][1]) {
-          max = ht[k[i]][1];
-          min = ht[k[i - 1]][1];
-        } else {
-          max = ht[k[i]][i - 1];
-          min = ht[k[i]][1];
-        }
-        for (let j = min + 1; j < max; j++) {
-          if (this.grid[ht[k[i]][0]][j] == 0) {
-            full = false;
-          } else if (this.grid[ht[k[i]][0]][j] != this.turn) {
-            del.push({ x: ht[k[i]][0], y: j });
-          }
-        }
-        if (full) {
-          for (let j = 0; j < del.length; j++) {
-            this.grid[del[j].x][del[j].y] = 0;
+    //Filter ht
+    for (let x in ht) {
+      if (ht[x] >= ht[trig]) {
+        nHt[x] = ht[x];
+      }
+    }
+
+    //Check if full on inside and fill del arr
+    const k = Object.keys(nHt);
+    k.forEach((e, i) => {
+      if (i != 0) {
+        //Get values from keys
+        let x = (e - (e % 10)) / 10;
+        let y = e % 10;
+        let prevX = (k[i - 1] - (k[i - 1] % 10)) / 10;
+        let prevY = k[i - 1] % 10;
+
+        if (x === prevX) {
+          for (let i = prevY + 1; i < y; i++) {
+            if (this.grid[x][i] == 0) {
+              full = false;
+            } else if (this.grid[x][i] !== this.turn) {
+              del.push([x, i]);
+            }
           }
         }
       }
+    });
+
+    if (full) {
+      del.forEach(e => {
+        this.grid[e[0]][e[1]] = 0;
+      });
     }
   }
 }
 
 //FOR TESTING
-//   const grid = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 1, 1, 0, 0, 0, 0],
-//     [0, 0, 1, -1, -1, 1, 0, 0, 0],
-//     [0, 0, 0, 1, -1, 1, 0, 0, 0],
-//     [0, 0, 0, 0, 1, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0]
-//   ];
+// const grid = [
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 1, -1, 0, 0, 0, 0],
+//   [0, 0, 1, -1, 1, -1, 0, 0, 0],
+//   [0, 0, 0, 1, -1, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+// ];
 
-//   let check = new Check(grid, 3, 5, 1);
-//   check.calc();
-//   console.log(check.grid);
+// let check = new Check(grid, 4, 3, -1);
+// check.calc();
+// console.log(check.grid);
 
 export default Check;
